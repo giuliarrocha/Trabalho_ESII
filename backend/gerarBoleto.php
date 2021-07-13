f28efadfa465062140a354e2fdd9754fdba33daa
Larissa Mayumi <larissa.barela@gmail.com>
2021-07-13 16:54:51 -0300

    Boleto

---
+++ backend/gerarBoleto.php
@@ -0,0 +1,11 @@
+<?php
+    
+    $txtnome= htmlentities("PÂO Çúáçar");
+    
+    //$aa = strtr(utf8_decode($txtnome), utf8_decode(' àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'), '_aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
+    
+    echo $txtnome;
+    $data = "13%2F07%2F2021";
+    $data = "13%2F07%2F2021";
+//header('Location: http://www.sicadi.com.br/mhouse/boleto/boleto3.php?numero_banco=341-7&local_pagamento=PAG%C1VEL+EM+QUALQUER+BANCO+AT%C9+O+VENCIMENTO&cedente=Loja+Ltda&data_documento='.$data.'&numero_documento=DF+'.$cod_boleto.'&especie=&aceite=S&data_processamento='.$data_atual.'&uso_banco=Caixa&carteira=179&especie_moeda=Real&quantidade=&valor=&vencimento='.$vencimento.'&agencia=0049&codigo_cedente=10201-5&meunumero=00010435&valor_documento='.$valor.'&instrucoes=Taxa+de+visita+de+suporte%0D%0AAp%F3s+o+vencimento+R%24+0%2C80+ao+dia&mensagem1=&mensagem2=&mensagem3=ATEN%C7%C3O%3A+N%C3O+RECEBER+AP%D3S+5+DIAS+DO+VENCIMENTO'.$txtnome.'&sacado=&Submit=Enviar', TRUE, 301);
+?>
